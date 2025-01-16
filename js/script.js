89d37fac83b8e2e28e40ef6153a0fda5c3641ce5
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2025-01-16 01:18:02 -0300

    finalizando perguntas

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function ControleCard(){
+  let cardAberto = document.querySelectorAll('.card_aberto');
+  let cardSeta = document.querySelectorAll('.card_seta');
+
+  for(let i=0;i<cardAberto.length;i++){
+    cardSeta[i].addEventListener('click', function(){
+      if(cardAberto[i].classList.contains('fechado')){
+        cardAberto[i].classList.add('aberto');
+        cardAberto[i].classList.remove('fechado');
+        cardSeta[i].classList.add('bi-chevron-up');
+        cardSeta[i].classList.remove('bi-chevron-down');
+      }
+      else{
+        cardAberto[i].classList.remove('aberto');
+        cardAberto[i].classList.add('fechado');
+        cardSeta[i].classList.remove('bi-chevron-up');
+        cardSeta[i].classList.add('bi-chevron-down');
+      }
+    });
+  }
+}
+
+ControleCard();
