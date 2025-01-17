97aaa7ffaf3f486ad0241900ef9a5c238d476b69
Davi Nascimento <davi.nascimento0907@gmail.com>
2025-01-16 21:48:33 -0300

    update main

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  function ControleCard(){
+    let cardAberto = document.querySelectorAll('.card_aberto');
+    let cardSeta = document.querySelectorAll('.card_seta');
+  
+    for(let i=0;i<cardAberto.length;i++){
+      cardSeta[i].addEventListener('click', function(){
+        if(cardAberto[i].classList.contains('fechado')){
+          cardAberto[i].classList.add('aberto');
+          cardAberto[i].classList.remove('fechado');
+          cardSeta[i].classList.add('bi-chevron-up');
+          cardSeta[i].classList.remove('bi-chevron-down');
+        }
+        else{
+          cardAberto[i].classList.remove('aberto');
+          cardAberto[i].classList.add('fechado');
+          cardSeta[i].classList.remove('bi-chevron-up');
+          cardSeta[i].classList.add('bi-chevron-down');
+        }
+      });
+    }
+  }
+  
+  ControleCard();
